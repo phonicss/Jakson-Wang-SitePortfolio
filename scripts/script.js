b424cd512fdacd78856e64a96c601a9a5ffceb16
Zaza <121345417+phonicss@users.noreply.github.com>
2023-03-18 16:23:55 +0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,62 +2,64 @@
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollSmoother.create({
-    wrapper: "wrapper",
-    content: "content",
-    smooth: 1.5,
-    effects: true,
-})
+if (ScrollTrigger.isTouch !== 1) {
+    ScrollSmoother.create({
+        wrapper: "wrapper",
+        content: "content",
+        smooth: 1.5,
+        effects: true,
+    })
 
-gsap.fromTo(".hero-section", {opacity: .85}, {
-    opacity: 0,
-    scrollTrigger: {
-        trigger: ".hero-section",
-        start: "center",
-        end: "1200",
-        scrub: true,
-    }
-})
+    gsap.fromTo(".hero-section", {opacity: .85}, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: ".hero-section",
+            start: "center",
+            end: "1200",
+            scrub: true,
+        }
+    })
 
-gsap.fromTo(".hero-section", {opacity: .85}, {
-    opacity: 0,
-    scrollTrigger: {
-        trigger: ".hero-section",
-        start: "center",
-        end: "1200",
-        scrub: true,
-    }
-})
+    gsap.fromTo(".hero-section", {opacity: .85}, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: ".hero-section",
+            start: "center",
+            end: "1200",
+            scrub: true,
+        }
+    })
 
-let itemsL = gsap.utils.toArray(".gallery__left .gallery__item")
+    let itemsL = gsap.utils.toArray(".gallery__left .gallery__item")
 
-itemsL.forEach(item => {
-gsap.fromTo(item,{ x: -100,opacity: 0 }, {
-    opacity: 1, x: 0,
-    scrollTrigger: {
-      trigger: item,
-      start: "-850",
-      end: "-100",
-      scrub: true,
+    itemsL.forEach(item => {
+    gsap.fromTo(item,{ x: -100,opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-850",
+          end: "-100",
+          scrub: true,
 
-    }  
-  })
-})
+        }  
+      })
+    })
 
-let itemsR = gsap.utils.toArray(".gallery__right .gallery__item")
+    let itemsR = gsap.utils.toArray(".gallery__right .gallery__item")
 
-itemsR.forEach(item => {
-gsap.fromTo(item,{ x: 100,opacity: 0 }, {
-    opacity: 1, x: 0,
-    scrollTrigger: {
-      trigger: item,
-      start: "-850",
-      end: "-100",
-      scrub: true,
+    itemsR.forEach(item => {
+    gsap.fromTo(item,{ x: 100,opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-850",
+          end: "-100",
+          scrub: true,
 
-    }  
-  })
-});
+        }  
+      })
+    });
+};
 
 
 
